3b6f7b96a4c0c662d455729df60e78fa04e43442
Malven <marcusalven@gmail.com>
2019-03-23 20:57:59 +0100

    env vars

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,14 @@ import 'firebase/firestore';
 import 'firebase/database';
 import 'firebase/auth';
 
-const config = {};
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER
+};
 
 firebase.initializeApp(config);
 
